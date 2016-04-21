bea3a531abe3ea5414399f15d6a6f3375bcd0a20
keryhu <keryhu@hotmail.com>
2016-04-21 10:06:33 +0800

    u

--- demo/src/main/java/com/xdidian/keryhu/domain/AuthUser.java
+++ demo/src/main/java/com/xdidian/keryhu/domain/AuthUser.java
@@ -2,7 +2,7 @@ package com.xdidian.keryhu.domain;
 
 import java.io.Serializable;
 import java.util.List;
-
+import java.util.stream.Collectors;
 
 import lombok.Data;
 
@@ -22,5 +22,19 @@ public class AuthUser implements Serializable {
 	private String id;
 	private String password;
 	private List<Role> roles;
+	
+	public void addRole(Role role) {
+		this.roles.add(role);
+	}
+	
+	public boolean hasRole(Role role) {
+        return (this.roles.contains(role));
+    }
+	 public void removeRole(Role role) {
+		 this.roles=this.roles.stream()
+				 //将符合条件的相同role去掉
+		           .filter(e->!e.equals(role))
+		           .collect(Collectors.toList());		
+		}
 	
 }
