875f7806c45b9094e1aa3925777e4c91c43b8d5b
keryhu <keryhu@hotmail.com>
2016-05-03 16:48:05 +0800

    add random

--- demo/src/main/java/com/xdidian/keryhu/util/RestRandomCode.java
+++ demo/src/main/java/com/xdidian/keryhu/util/RestRandomCode.java
@@ -1,8 +1,8 @@
 package com.xdidian.keryhu.util;
 
-import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -17,30 +17,71 @@ import org.slf4j.LoggerFactory;
 
 public final class RestRandomCode  {
 	
-	private RestRandomCode(){}
+	private RestRandomCode(){};
+	
 	
 	private static final Logger logger=LoggerFactory.getLogger(RestRandomCode.class);
+		
+	private static final List<String> code=new ArrayList<String>();
 	
-	private final static String code[]={"jsixm233ms899","sdmx34mi9xm23",
-			"-0x=,.ws872kx","jxm23msx,923s","js99823mmx..sd9","jsixm23ls..mci",
-			"msmdlx23,,xm323","sdmx23,id2e2s","msdkx932l9023s","sdmxiems,,soe2,",
-			"sdmix,2,3isd2","msdk23,99203,sd"};
+	/**
+	 * 初始化40个由12个字符串组成的数组。
+	 */
+    static {
+    	for (int i=0;i<40;i++){
+    		code.add(generateRandom(12));
+    	}
+		
+	}
 	
 	
-	
-	
-	public static String createRandomCode(){
+    /**
+     * 
+    * @Title: createCode
+    * @Description: TODO(从随机数的池子里面，随机挑选一个字符串)
+    * @param @return    设定文件
+    * @return String    返回类型
+    * @throws
+     */
+	public static String createCode(){
 		Random ra =new Random();
-		
-		String m=code[ra.nextInt(code.length)];
+		String m=code.get(ra.nextInt(code.size()));
 		logger.info("获取到的随机数组是： "+m);
-		
 		return m;
 		
 	}
 	
+	/**
+	 * 
+	* @Title: containCode
+	* @Description: TODO(看看此字符串是否存在于池子里)
+	* @param @param random
+	* @param @return    设定文件
+	* @return boolean    返回类型
+	* @throws
+	 */
 	public static boolean containCode(String random){
-		return Arrays.asList(code).contains(random);
+		return code.contains(random);
+	}
+	
+	/**
+	 * 
+	* @Title: generateRandom
+	* @Description: TODO(生成固定长度的随机字符串)
+	* @param @param length
+	* @param @return    设定文件
+	* @return String    返回类型
+	* @throws
+	 */
+	public static String generateRandom(int length){
+		StringBuffer buffer = new StringBuffer("0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ<>?:{}[]#$@%^&*()"); 
+		StringBuffer sb = new StringBuffer();   
+        Random random = new Random();   
+        int range = buffer.length();   
+        for (int i = 0; i < length; i ++) {   
+            sb.append(buffer.charAt(random.nextInt(range)));   
+        }   
+        return sb.toString();   
 	}
 	
 }
