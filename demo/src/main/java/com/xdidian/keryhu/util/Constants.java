8bea06fa6135c1d7d923f49d54ef5d87719f9e67
keryhu <keryhu@hotmail.com>
2016-05-04 09:24:41 +0800

    add constants

---
+++ demo/src/main/java/com/xdidian/keryhu/util/Constants.java
@@ -0,0 +1,21 @@
+package com.xdidian.keryhu.util;
+
+/**
+ * 
+* @ClassName: Constants
+* @Description: TODO(设置整个系统的常量 )
+* @author keryhu  keryhu@hotmail.com
+* @date 2016年5月4日 上午9:18:07
+ */
+public  final class Constants {
+	
+	private Constants(){
+		//不允许实例化
+	}
+	
+	public static final boolean LOGIN_SUCCESS=true;   //表示登录成功的信号
+	
+	public static final String  READ_AND_WRITE_RESOURCE_ID="ReadAndWriteResource";   //可读写的resourceID
+	
+
+}
