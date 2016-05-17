80ca3bdbe09ed4894cff4f6a181210890c091265
keryhu <keryhu@hotmail.com>
2016-05-17 13:19:52 +0800

    update emailacitvateDto and add passwordResetDto

--- demo/src/main/java/com/xdidian/keryhu/domain/PasswordResetDto.java
+++ demo/src/main/java/com/xdidian/keryhu/domain/PasswordResetDto.java
@@ -15,27 +15,25 @@ import lombok.Data;
 
 /**
  * 
-* @ClassName: EmailActivatedDto
-* @Description: TODO(用于邮箱激活，从注册后台发送message出去的dto包含了：email，token 和 过期时间，)
+* @ClassName: PasswordResetDto
+* @Description: TODO(忘记密码，重设密码过程中需要用到的，相互之间传递message的dto)
 * @author keryhu  keryhu@hotmail.com
-* @date 2016年5月5日 下午7:34:55
+* @date 2016年5月17日 下午1:10:58
  */
 @Data
-public class EmailActivatedDto implements Serializable {
- 
-	private static final long serialVersionUID = -1178102023977336785L;
+public class PasswordResetDto implements Serializable  {
+
+	
+	private static final long serialVersionUID = 2456549940699014529L;
 	
 	private String email;
-	private String emailToken;   //邮箱激活的验证码
-	
-	private String resendToken;  // “重新发送激活邮件”Url中的token
-	
-	private String reregisterToken;  // “重新注册”Url中的token
+	private String passwordToken ;   // 验证重设密码url中的token
+	private String resendToken ;     //“重新发送邮件”url中的token
+	private String userId;           // 需要被重设密码的user的id
 	
 	@DateTimeFormat(iso=ISO.DATE_TIME)
 	@JsonDeserialize(using = LocalDateTimeDeserializer.class)
 	@JsonSerialize(using = LocalDateTimeSerializer.class)
-	private LocalDateTime expireDate;   //用户email激活的截止时间
-	
-	
+	private LocalDateTime expireDate;   //重设密码链接有效期的截止时间
+
 }
