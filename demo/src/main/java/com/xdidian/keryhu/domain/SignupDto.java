1ad01258e496f199de3559c92984ac71485d9839
keryhu <keryhu@hotmail.com>
2016-07-28 15:49:07 +0800

    update signup code

--- demo/src/main/java/com/xdidian/keryhu/domain/SignupDto.java
+++ demo/src/main/java/com/xdidian/keryhu/domain/SignupDto.java
@@ -7,29 +7,28 @@ import lombok.Data;
 
 /**
 * @ClassName: PropertyRegisterDto
-* @Description: TODO(此类是一个dto，用来将用户注册的数据远程提交给useraccount service，然后再转为user数据保存数据库)
+* @Description: TODO(此类是一个dto，用来将用户注册的数据远程提交给useraccount service，然后再转为user数据保存数据库
+* 只单纯的提交email，phone和password，其他信息都没有)
 * @author keryhu  keryhu@hotmail.com
 * @date 2016年4月21日 下午4:51:51
  */
 @Data
 @AllArgsConstructor
-public class PropertyRegisterDto implements Serializable {
+public class SignupDto implements Serializable {
 
 	private static final long serialVersionUID = 2857339497829739963L;
 	
 	private String email;
 	private String phone;
 	private String password;
-	private String companyName;  //公司名字
-	private String directName;  //负责人姓名
 	
-	public PropertyRegisterDto() {
+	
+	public SignupDto() {
 		// TODO Auto-generated constructor stub
 		this.email=null;
 		this.phone=null;
 		this.password=null;
-		this.companyName=null;
-		this.directName=null;
+		
 		
 	}
 
