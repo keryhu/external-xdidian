1af3fac5a7c440365fbbdad736f494806992c059
keryhu <keryhu@hotmail.com>
2016-07-28 16:43:53 +0800

    modify the roles

--- demo/src/main/java/com/xdidian/keryhu/domain/Role.java
+++ demo/src/main/java/com/xdidian/keryhu/domain/Role.java
@@ -21,8 +21,11 @@ public enum Role {
 	  ROLE_DEFAULT,        //默认类型，值可以读写，自身的数据
 	 
 	  ROLE_COMPANY_ADMIN,          // 客户公司的company admin 公司的管理员
-	  
-	  ROLE_SOME_DEPARTMENT    ;   //公司的拥有读取某些部门数据的roles
+	  ROLE_XDIDIAN_ADMIN,         // 新地点公司的最高的管理员
+	  ROLE_XDIDIAN_SERVICE,       // 新地点公司的客服人员权限
+	  ROLE_SOME_DEPARTMENT    ;   //公司的拥有读取某些部门数据的roles，注意针对的是客户，所在的公司，用户能过读取某些部门权限
+	
+	
 	
 	
 	/**
@@ -34,6 +37,8 @@ public enum Role {
 	  static{
 		  roleMap.put("ROLE_SERVICE", ROLE_DEFAULT);
 		  roleMap.put("ROLE_COMPANY_ADMIN", ROLE_COMPANY_ADMIN);
+		  roleMap.put("ROLE_XDIDIAN_ADMIN", ROLE_XDIDIAN_ADMIN);
+		  roleMap.put("ROLE_XDIDIAN_SERVICE", ROLE_XDIDIAN_SERVICE);
 		  roleMap.put("ROLE_SOME_DEPARTMENT", ROLE_SOME_DEPARTMENT);
 		   
 		  
