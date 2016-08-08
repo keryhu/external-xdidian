a7658376b1f8ed910d93e43ef21dfa41a1e5b20c
keryhu <keryhu@hotmail.com>
2016-08-08 16:50:33 +0800

    add role hiretach

--- demo/src/main/java/com/xdidian/keryhu/util/Constants.java
+++ demo/src/main/java/com/xdidian/keryhu/util/Constants.java
@@ -13,11 +13,13 @@ public  final class Constants {
 		//不允许实例化
 	}
 	
-	
 	public static final String  READ_AND_WRITE_RESOURCE_ID="ReadAndWriteResource";   //可读写的resourceID
     
     //如果email激活码过期，则前台需要返回注册页面
     public static final String EMAIL_ACTIVATE_EXPIRED="emailActivateExpired";
+    
+    //定义一个使用新地点平台role，排序
+    public static final String ROLE_HIERARCHY="ROLE_XDIDIAN_ADMIN>ROLE_XDIDIAN_SERVICE ROLE_XDIDIAN_SERVICE>ROLE_COMPANY_ADMIN ROLE_COMPANY_ADMIN>ROLE_SOME_DEPARTMENT  ROLE_SOME_DEPARTMENT>ROLE_DEFAULT";
 	
 
 }
