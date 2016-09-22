7f6617f9e8e4bfd389b78703983c19f85b159483
keryhu <keryhu@hotmail.com>
2016-09-22 08:44:23 +0800

    add image service

--- demo/src/main/java/com/xdidian/keryhu/service/FileService.java
+++ demo/src/main/java/com/xdidian/keryhu/service/FileService.java
@@ -1,33 +1,22 @@
 package com.xdidian.keryhu.service;
 
-import java.io.File;
 
-import javax.activation.MimetypesFileTypeMap;
+import org.springframework.util.Assert;
+import org.springframework.web.multipart.MultipartFile;
 
-public class FileService {  /**
-	   * 
-	  * @Title: isImage
-	  * @Description: TODO(支持文件格式“jpeg,jpg”，“png”，“tif”，“tiff”,"bmp","gif"
-	  * 　主流的图片格式都已经支持了，且验证过了。)
-	  * @param @param file
-	  * @param @return    设定文件
-	  * @return boolean    返回类型
-	  * @throws
-	   */
-	  
-	  
-	  public boolean isImage(File file){
-	    String mimetype=new MimetypesFileTypeMap().getContentType(file);
-	    String type=mimetype.split("/")[0];
-	    if(type.equals("image")){
-	      return true;
-	    }
-	    else{
-	      return false;
-	    }
-	  }
-	  
-	
-	
+public class FileService {
+	/**
+	 * 
+	 * @Title: isImage @Description:
+	 * TODO(支持文件格式“jpeg,jpg”，“png”，“tif”，“tiff”,"bmp","gif"
+	 * 主流的图片格式都已经支持了，且验证过了。) @param @param file @param @return 设定文件 @return
+	 * boolean 返回类型 @throws
+	 */
+
+	public boolean isImage(MultipartFile file) {
+		Assert.isTrue(!(file == null || file.isEmpty()), "上传文件不能为空");
+		String type = file.getContentType().split("/")[0];
+		return type.equals("image");
+	}
 
 }
